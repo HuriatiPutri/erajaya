d31befc8bd4207f4f6369df7cf38ea250c1a6fc9
Putri Huriati <putrihuriati7@gmail.com>
2024-01-12 18:43:19 +0700

    styling button

--- src/component/elements/button/Button.jsx
+++ src/component/elements/button/Button.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types'
+import styles from './styles.module.scss'
 
 export default function Button ({ children, onClick }) {
   return (
     <button
-      onClick={onClick}
-      className="flex flex-row bg-blue-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      className={styles.btn}
+      onClick={onClick}>
       {children}
     </button>
   )
